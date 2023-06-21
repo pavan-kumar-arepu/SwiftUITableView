a4d074e2f46feec0b183f1c9ede6bdde75d3c03f
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-06-21 15:24:42 +0200

    LeaderListView Created
    LeaderListView Created to maintain SRP - Single Responsbility Principle

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -11,54 +11,8 @@ import FirebaseRemoteConfig
 
 struct ContentView: View {
     
-    @State var remoteLeaders: [Leader] = []
-    var leaderVM = LeaderViewModel()
-    
-    @State private var isLoading = false
-    
     var body: some View {
-        
-        NavigationView {
-            ZStack {
-                LinearGradient(
-                    gradient: Gradient(colors: [
-                        Color.orange.opacity(0.5),
-                        Color.white.opacity(0.5),
-                        Color.green.opacity(0.5)
-                    ]),
-                    startPoint: .top,
-                    endPoint: .bottom
-                )
-                .edgesIgnoringSafeArea(.all)
-                
-                if isLoading {
-                    ProgressView() // Show loading indicator while fetching data
-                } else {
-                    Text("Data Loaded!") // Show content when data is loaded
-                    List(remoteLeaders, id: \.name) { (leader: Leader) in
-                        NavigationLink(destination: LeaderDetail(leader: leader)) {
-                            LeaderRow(leader: leader)
-                        }
-                    }
-                    .background(Color.clear)
-                    .listStyle(InsetGroupedListStyle())
-                    .navigationBarTitle("Indian Leaders")
-                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-                }
-            }
-            .background(Color.clear)
-        }
-        .onAppear {
-            isLoading = true
-            FirebaseRemoteConfigManager.shared.fetchRemoteConfigData { leaders in
-                isLoading = false
-                if let leaders = leaders {
-                    remoteLeaders = leaders
-                }
-            }
-        }
-        .background(Color.clear)
-        .statusBar(hidden: true)
+        LeaderListView()
     }
 }
 
---
+++ View/LeaderListView.swift
@@ -0,0 +1,66 @@
+//
+//  LeaderListView.swift
+//  IndianLeaders
+//
+//  Created by Pavankumar Arepu on 21/06/23.
+//
+
+import SwiftUI
+
+struct LeaderListView: View {
+    
+    @State var remoteLeaders: [Leader] = []
+    var leaderVM = LeaderViewModel()
+    
+    @State private var isLoading = false
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        Color.orange.opacity(0.5),
+                        Color.white.opacity(0.5),
+                        Color.green.opacity(0.5)
+                    ]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.all)
+                
+                if isLoading {
+                    ProgressView() // Show loading indicator while fetching data
+                } else {
+                    Text("Data Loaded!") // Show content when data is loaded
+                    List(remoteLeaders, id: \.name) { (leader: Leader) in
+                        NavigationLink(destination: LeaderDetail(leader: leader)) {
+                            LeaderRow(leader: leader)
+                        }
+                    }
+                    .background(Color.clear)
+                    .listStyle(InsetGroupedListStyle())
+                    .navigationBarTitle("Indian Leaders")
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                }
+            }
+            .background(Color.clear)
+        }
+        .onAppear {
+            isLoading = true
+            FirebaseRemoteConfigManager.shared.fetchRemoteConfigData { leaders in
+                isLoading = false
+                if let leaders = leaders {
+                    remoteLeaders = leaders
+                }
+            }
+        }
+        .background(Color.clear)
+        .statusBar(hidden: true)
+    }
+}
+
+struct LeaderListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LeaderListView()
+    }
+}
